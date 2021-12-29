bb036de85d890bf8d9717cb69005d3fe30449ecd
Adir Buskila <Adirbu98@gmail.com>
2021-12-29 17:36:51 +0200

    make to list

--- js/apps/keep/cmps/note-to-do.jsx
+++ js/apps/keep/cmps/note-to-do.jsx
@@ -1,3 +1,7 @@
-import function MakeToDoList(todos) {
-    
+export function MakeToDoList(todos) {
+    const Todo = todos.todos.map((todo,idx) =>{
+            return <p key={idx}>{todo.txt}</p>
+        })
+    return Todo
+        
 }
