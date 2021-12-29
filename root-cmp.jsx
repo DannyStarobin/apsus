248be46678958d2dfabca196ef70751681b78479
Adir Buskila <Adirbu98@gmail.com>
2021-12-29 10:54:38 +0200

    commit

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,7 +1,8 @@
-import {AppHome} from './pages/home.jsx'
-import {MissBook} from './pages/MissBook.jsx'
-import {MissEmail} from './pages/MissEmail.jsx'
-import {MissKeep} from './pages/MissKeep.jsx'
+import { AppHome } from './js/pages/app-home.jsx'
+
+import { MissBook } from './js/apps/book/pages/book-index.jsx'
+import {MissEmail} from './js/apps/mail/pages/mail-index.jsx'
+import { MissKeep } from './js/apps/keep/pages/note-index.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
@@ -9,12 +10,9 @@ const { Route, Switch } = ReactRouterDOM
 export function App() {
     return <section className="app">
         <Router>
-            <header>
-                <h1>AppSus</h1>
-            </header>
             <main>
                 <Switch>
-                    <Route component={Home} path="/" />
+                    <Route component={AppHome} path="/" />
                     <Route component={MissBook} path="/book" />
                     <Route component={MissEmail} path="/email" />
                     <Route component={MissKeep} path="/keep" />
