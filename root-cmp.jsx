c5252ded9b4bbb6e86a849cff6931d6bcd5ad4f8
Adir Buskila <Adirbu98@gmail.com>
2021-12-29 09:59:38 +0200

    first commit

---
+++ root-cmp.jsx
@@ -0,0 +1,13 @@
+import {Home} from './pages/home.jsx'
+
+export function App() {
+    return <section className="app">
+        <header>
+            <h1>My App</h1>
+        </header>
+        <main>
+            <Home/>
+        </main>
+    </section>
+}
+
