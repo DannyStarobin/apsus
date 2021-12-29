552214efa9b8948d9bca4250568eae7a300b5f34
Danny Starobin <dannystar1@gmail.com>
2021-12-29 17:37:04 +0200

    search input

---
+++ js/cmps/search-input.jsx
@@ -0,0 +1,47 @@
+export class SearchInput extends React.Component {
+
+    state = {
+        app: null
+
+    }
+
+
+
+    onSubmitFilter = (ev) => {
+        ev.preventDefault();
+        this.props.onSetFilter(this.state.filterBy);
+        this.cleanForm();
+    };
+
+
+
+    handleChange = ({ target }) => {
+        const field = target.name;
+        const value = (target.type === 'number') ? +target.value : target.value;
+        this.setState((prevState) => ({ filterBy: { ...prevState.filterBy, [field]: value } }), () => {
+            this.props.onSetFilter(this.state.filterBy)
+        })
+    }
+
+
+
+
+    render() {
+        if (!this.state.app) return <React.Fragment></React.Fragment>
+        return (
+            <form className="book-filter" onSubmit={this.onSubmitFilter}>
+                <label htmlFor="by-title">By Title:</label>
+                <input
+                    type="text"
+                    id="by-title"
+                    name="title"
+                    value={title}
+                    onChange={this.handleChange}
+                />
+            </form>
+        )
+    }
+
+
+
+}
