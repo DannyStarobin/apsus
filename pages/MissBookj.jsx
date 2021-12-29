910ece1cc7e5bbb1c25fdf3916c0bfbbdec979c8
Danny Starobin <dannystar1@gmail.com>
2021-12-29 10:16:31 +0200

    pages

---
+++ pages/MissBookj.jsx
@@ -0,0 +1,13 @@
+export class MissBook extends React.Component {
+
+
+    render() {
+        return (
+            <section className="about">
+                <h1>MissBook</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit earum tenetur, accusantium adipisci, provident reprehenderit eaque similique quia nemo commodi et laboriosam ab est sunt dolor velit repellat illum placeat.</p>
+            </section>
+        )
+    }
+
+}
