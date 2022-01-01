539aa63f66d4928db782d0895dd792ba196d218a
Adir Buskila <Adirbu98@gmail.com>
2022-01-01 20:30:29 +0200

    home page and fonts

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -7,6 +7,7 @@ export class AppHome extends React.Component {
             <section className="app-home">
                 <h2>Welcome Back to AppSus!</h2>
                 <h3>Here you can manage your apps easily</h3>
+                <img className="home-img" src="./assets/img/Technology.jpg" />
             </section>
         )
     }
