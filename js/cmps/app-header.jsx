892777e6563e902e62ddba0f223b89e04bd202cb
Adir Buskila <Adirbu98@gmail.com>
2022-01-01 20:55:54 +0200

    css

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -21,10 +21,10 @@ export class AppHeader extends React.Component {
 
         return (
             <header className="app-header" >
-                <div className="logo">Apps Place</div>
-
-        
-                
+                <div className="logo">
+                    <p>Apps Place</p>
+                    <img className="logo-img" src="assets/img/logo.png" />
+                    </div>
                 <nav className="main-nav">
                     <NavLink activeClassName="my-active" exact to="/">Home</NavLink>
                     <NavLink to="/email">Mail</NavLink>
